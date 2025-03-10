b01a4c8a5c24e30f8c5cc8dcdcb13748f4bf07f5
omilli <oli.millington@gmail.com>
2025-03-10 19:24:45 +0700

    update events example

--- examples/lib/concepts/render/events.ts
+++ examples/lib/concepts/render/events.ts
@@ -1,15 +1,17 @@
 import { render } from "@hellajs/core";
 
 render(
-  {
+  () => ({
     tag: "button",
     classes: "btn",
-    onclick: () => { console.log("clicked"); },
+    onclick: () => {
+      console.log("clicked");
+    },
     data: {
       id: "submit-btn",
       testid: "submit",
     },
     content: "Click me",
-  },
-  "#app",
+  }),
+  "#app"
 );
